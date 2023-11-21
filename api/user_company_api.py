37c9f3bbe0c17990760743c7dd020151e960ec19
Nividc <136467872+nividc@users.noreply.github.com>
2023-11-21 09:28:38 +0530

    Update user_company_api.py

--- api/user_company_api.py
+++ api/user_company_api.py
@@ -5,11 +5,9 @@ from models.company import Company
 class UserCompanyAPI:
     def create_user(self, name, role):
         new_user = User(name, role)
-        # Logic to store the new user in a database or data structure
         return new_user
 
     def create_company(self, name):
         new_company = Company(name)
-        # Logic to store the new company in a database or data structure
         return new_company
 
